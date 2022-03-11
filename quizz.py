a0c99c2bbd3eed0fd7ff6df80ba1a48c448bdc7a
Jordan BEAUX <55492676+jordanbeaux@users.noreply.github.com>
2022-03-11 15:06:30 +0100

    Completion déroulé quizz

--- quizz.py
+++ quizz.py
@@ -7,5 +7,24 @@ class Quizz:
         self.questions = liste_questions if isinstance(liste_questions, Question) else None
         self.score = 0
 
-    def __check_response(self, reponse: Reponse):
+    def __check_response(self, reponse: Reponse, question: Question):
+        if reponse == question.correct_answers:
+            print("Bravo c'est la bonne réponse !")
+            self.score += 1
+            print(f'Votre score est désormais de : {self.score} points.')
+        else:
+            print(f"Ce n'est pas la bonne réponse. La bonne réponse était : {question.correct_answers}")
 
+    def deroulee_quizz(self):
+        for question in self.questions:
+            print(f'Question n° {list.index(question)} sur {len(self.questions)} :')
+            print(f'{question.question}')
+            print("Voici les propositions : ")
+            print(f'{reponse} \n' for reponse in question.answers)
+            print("Il y a plusieurs bonnes réponses" if question.multiple_answers else "Il n'y a qu'une bonne réponse")
+
+            reponse_utilisateur = input("Entrez vos réponses, séparer par des virgules")
+
+            Quizz.__check_response(reponse=reponse_utilisateur, question=question)
+
+        print(f'Le quizz est terminée. Votre score final est de {self.score}')
